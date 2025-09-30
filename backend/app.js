311cfd4826891a85b66dfcc5add72164e82e8f28
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-09-30 15:59:28 +0200

    Mise en place d'un fichier server.js et modification du package.json

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,27 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
+import userRoutes from "./routes/userRoutes.js";
+import articleRoutes from "./routes/articleRoutes.js";
+import concertRoutes from "./routes/concertRoutes.js";
+import donationRoutes from "./routes/donationRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API backend en route 🚀");
-});
+app.use(express.json());
+app.use(cookieParser());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use("/api/users", userRoutes);
+app.use("/api/articles", articleRoutes);
+app.use("/api/concerts", concertRoutes);
+app.use("/api/donations", donationRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+app.use(errorHandler);
+
+export default app;
