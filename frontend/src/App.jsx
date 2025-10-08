40fdd0ef8bddf69ac6767b209fb6eff47e1a7d3d
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-10-08 13:40:26 +0200

    suppression de la page intro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Intro from "./pages/Intro";
+
 
 
 function App() {
   return (
     <div className="min-h-screen bg-[#0A0A0A] text-[#F2F2F2]">
       <Routes>
-        <Route path="/" element={<Intro />} />
+        <Route path="/" element={<Home />} />
         
       </Routes>
     </div>
