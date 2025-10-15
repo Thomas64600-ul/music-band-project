8046cf24b46d9e54e1013ea2a6a6ac6782c540ed
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-10-15 14:26:12 +0200

    Mise en place du fichier ProtectedRoute

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function ProtectedRoute({ children, requireAdmin=false }) {
+  const { user, loading, isAdmin } = useAuth();
+
+  if (loading) return null; 
+
+  if (!user) return <Navigate to="/login" replace />;
+  if (requireAdmin && !isAdmin) return <Navigate to="/" replace />;
+
+  return children;
+}
