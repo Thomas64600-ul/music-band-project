427c381ef3f43a932d9dbb2f6500f91d3c7d6ee9
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-10-20 10:17:57 +0200

    modification de uploadMiddleware

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,42 +1,54 @@
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import { v2 as cloudinary } from "cloudinary";
-import "../config/cloudinary.js"; 
+import express from "express";
+import {
+  addArticle,
+  fetchArticles,
+  fetchArticleById,
+  editArticle,
+  removeArticle,
+} from "../controllers/articleController.js";
+
+import { validate } from "../middlewares/validationMiddleware.js";
+import { createArticleSchema, updateArticleSchema } from "../schemas/articleSchema.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import { authorizeRoles } from "../middlewares/roleMiddleware.js";
+import upload from "../middlewares/uploadMiddleware.js";
+
+const router = express.Router();
 
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: async (req, file) => {
-    const folder = req.baseUrl.includes("concerts")
-      ? "reveren_uploads/concerts"
-      : "reveren_uploads/articles";
-
-    return {
-      folder,
-      allowed_formats: ["jpg", "jpeg", "png", "webp"],
-      transformation: [{ width: 1200, crop: "limit" }],
-      resource_type: "image",
-    };
-  },
-});
+router.post(
+  "/",
+  protect,
+  authorizeRoles("admin"),
+  upload.single("image"),           
+  validate(createArticleSchema),
+  addArticle
+);
 
 
-const fileFilter = (req, file, cb) => {
-  const allowed = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
-  if (allowed.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Format d’image non supporté (jpg, png, webp uniquement)."));
-  }
-};
+router.get("/", fetchArticles);
 
 
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, 
-});
+router.get("/:id", fetchArticleById);
 
-export default upload;
+
+router.put(
+  "/:id",
+  protect,
+  authorizeRoles("admin"),
+  upload.single("image"),
+  validate(updateArticleSchema),
+  editArticle
+);
+
+
+router.delete(
+  "/:id",
+  protect,
+  authorizeRoles("admin"),
+  removeArticle
+);
+
+export default router;
 
 
