5ceb717b85d1d6779c10fd90a6b8e4458198bd5a
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-10-20 14:43:03 +0200

    Modification de app.js

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,16 @@ import compression from "compression";
 
 import { errorHandler } from "./middlewares/errorHandler.js";
 
+
 import userRoutes from "./routes/userRoutes.js";
 import articleRoutes from "./routes/articleRoutes.js";
 import concertRoutes from "./routes/concertRoutes.js";
 import donationRoutes from "./routes/donationRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+
+
+import musicRoutes from "./routes/musicRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -44,10 +48,10 @@ app.use(
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",                          
-      process.env.CLIENT_URL,                            
-      "https://music-band-project-frontend.onrender.com", 
-    ].filter(Boolean), 
+      "http://localhost:5173",
+      process.env.CLIENT_URL,
+      "https://music-band-project-frontend.onrender.com",
+    ].filter(Boolean),
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -58,8 +62,6 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-
 app.use(morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined"));
 
 
@@ -80,17 +82,20 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
+
 app.use("/api/users", userRoutes);
 app.use("/api/articles", articleRoutes);
 app.use("/api/concerts", concertRoutes);
 app.use("/api/donations", donationRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/musics", musicRoutes);
 
 
 app.use(errorHandler);
 
 export default app;
+
 
 
 
