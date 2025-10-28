716714b51c9c446acbc6bd6b1b4ab2baa1a2f239
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-10-28 09:23:17 +0100

    Modification de style du dashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -22,16 +22,31 @@ export default function AdminDashboard() {
         min-h-screen
         bg-[var(--bg)] text-[var(--text)]
         transition-colors duration-700 ease-in-out
+        relative overflow-hidden
       "
     >
+      
       <div
         className="
-          bg-[var(--bg-secondary)] border border-[var(--accent)]/40
-          rounded-2xl shadow-[0_0_25px_var(--accent)]/30
-          p-8 sm:p-10 w-full max-w-3xl text-center
+          absolute inset-0 -z-10
+          bg-[radial-gradient(circle_at_center,#B3122D33_0%,transparent_70%)]
+          blur-[150px] opacity-70
+        "
+      ></div>
+
+     
+      <div
+        className="
+          relative w-full max-w-3xl text-center
+          rounded-2xl shadow-lg 
+          bg-[color-mix(in_oklab,var(--bg)_95%,black_5%)]
+          border border-[rgba(179,18,45,0.35)]
+          shadow-[0_0_25px_rgba(179,18,45,0.35)]
+          transition-all duration-500
+          p-8 sm:p-10
         "
       >
-        <h1 className="text-3xl sm:text-4xl font-extrabold text-[var(--accent)] mb-6 drop-shadow-[0_0_12px_var(--accent)]">
+        <h1 className="text-3xl sm:text-4xl font-extrabold text-[var(--accent)] mb-6 drop-shadow-[0_0_10px_var(--accent)]">
           Tableau de bord Admin
         </h1>
 
@@ -112,7 +127,6 @@ export default function AdminDashboard() {
             GÉRER LES COMMENTAIRES
           </Button>
 
-       
           <Button
             onClick={() => navigate("/admin/users")}
             className="
